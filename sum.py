c76690a30c544c362eb614511b29b9a56b950ae8
anuradha dilip dwivedi <anuradha.dwivedi2021@gmail.com>
2025-08-17 14:51:08 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print("Array:", arr)
+print("Sum:", np.sum(arr))       # Sab elements ka sum
+print("Mean:", np.mean(arr))     # Average
+print("Max:", np.max(arr))       # Maximum
+print("Min:", np.min(arr))       # Minimum
---
+++ zero_onearray.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+zeros_arr = np.zeros((2, 3))  # 2 row × 3 column, sab 0
+ones_arr = np.ones((3, 3))    # 3 row × 3 column, sab 1
+
+print("Zeros Array:\n", zeros_arr)
+print("Ones Array:\n", ones_arr)
